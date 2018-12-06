9893fab71e667e45bd0a747d65f6a394c69c9777
stacysun <stacy@vanbex.com>
2018-12-06 11:00:54 -0800

    Add webpack

--- test/App.test.js
+++ test/App.test.js
@@ -1,6 +1,6 @@
-import App from '../src/components/App'
-import React from 'react'
-import { shallow } from 'enzyme'
+import App from '../src/components/App';
+import React from 'react';
+import { shallow } from 'enzyme';
 
 describe('App', () => {
     test('should match snapshot', () => {
